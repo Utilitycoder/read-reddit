4d6924683f4421325facda3734b9e8f6fe62e6b4
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-06-08 11:58:08 +0100

    chore: get the mvp running

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use reqwest::Client;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct SubredditListing {
+    pub data: SubredditChildren,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SubredditChildren {
+    children: Vec<RedditPost>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RedditPost {
+    data: PostData,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PostData {
+    title: String,
+    permalink: String,
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    const REDDIT_BASE_URL: &str = "https://www.reddit.com";
+    // user supplied subreddit from command line
+    let subreddit = std::env::args().nth(1).expect("please supply a subreddit");
+    println!("Subreddit: {}", subreddit);
+
+    // Construct the URL for the subreddit's hot topics
+    let url = format!("{}/r/{}/hot.json", REDDIT_BASE_URL, subreddit);
+
+    // Send the HTTP GET request
+    let client = Client::new();
+
+    // add user agent header
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(
+        reqwest::header::USER_AGENT,
+        reqwest::header::HeaderValue::from_static("rust"),
+    );
+    // add headers to client
+    let response_body = client.get(&url).headers(headers).send().await?.text().await?;
+
+    // Parse the json
+    let response: SubredditListing = serde_json::from_str(&response_body)?;
+
+
+    // Process and display the hot topics
+    for post in response.data.children {
+        println!("Title: {}", post.data.title);
+        println!("Permalink: {}{}", REDDIT_BASE_URL, post.data.permalink);
+        println!("---");
+    }
+
+    Ok(())
+}
